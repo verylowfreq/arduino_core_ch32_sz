4f25264fe1880e4b3f3a987864d74dea569cfb88
Mitsumine Suzu <60875431+verylowfreq@users.noreply.github.com>
2026-03-08 20:48:11 +0900

    Move intrrupt handlers from assembly startup to C code for LTO compile

--- system/CH32V20x/USER/ch32v20x_it.c
+++ system/CH32V20x/USER/ch32v20x_it.c
@@ -9,67 +9,131 @@
  *******************************************************************************/
 #include "ch32v20x_it.h"
 
-void NMI_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
-void HardFault_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
-void Ecall_U_Mode_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
-void Ecall_M_Mode_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+// void NMI_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+// void HardFault_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+// void Ecall_U_Mode_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+// void Ecall_M_Mode_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
 
-/*********************************************************************
- * @fn      NMI_Handler
- *
- * @brief   This function handles NMI exception.
- *
- * @return  none
- */
-void NMI_Handler(void)
-{
-}
+// /*********************************************************************
+//  * @fn      NMI_Handler
+//  *
+//  * @brief   This function handles NMI exception.
+//  *
+//  * @return  none
+//  */
+// void NMI_Handler(void)
+// {
+// }
 
-/*********************************************************************
- * @fn      HardFault_Handler
- *
- * @brief   This function handles Hard Fault exception.
- *
- * @return  none
- */
-void HardFault_Handler(void)
-{
-  while (1)
-  {
+// /*********************************************************************
+//  * @fn      HardFault_Handler
+//  *
+//  * @brief   This function handles Hard Fault exception.
+//  *
+//  * @return  none
+//  */
+// void HardFault_Handler(void)
+// {
+//   while (1)
+//   {
     
-  }
-}
+//   }
+// }
 
 
-/*********************************************************************
- * @fn      Ecall_U_Mode_Handler
- *
- * @brief   This function handles user mode ecall exception.
- *
- * @return  none
- */
-void Ecall_U_Mode_Handler(void)
-{
-  while(1)
-  {
+// /*********************************************************************
+//  * @fn      Ecall_U_Mode_Handler
+//  *
+//  * @brief   This function handles user mode ecall exception.
+//  *
+//  * @return  none
+//  */
+// void Ecall_U_Mode_Handler(void)
+// {
+//   while(1)
+//   {
     
-  }
-}
+//   }
+// }
 
-/*********************************************************************
- * @fn      Ecall_M_Mode_Handler
- *
- * @brief   This function handles mashine mode ecall exception.
- *
- * @return  none
- */
-void Ecall_M_Mode_Handler(void)
-{
-  while(1)
-  {
+// /*********************************************************************
+//  * @fn      Ecall_M_Mode_Handler
+//  *
+//  * @brief   This function handles mashine mode ecall exception.
+//  *
+//  * @return  none
+//  */
+// void Ecall_M_Mode_Handler(void)
+// {
+//   while(1)
+//   {
     
+//   }
+// }
+
+
+// Default interrupt handler definition
+
+void while1_handler(void) {
+  while(1) {
+
   }
-}
+};
 
 
+// Weak symbol for interrupts
 
+__attribute__((weak)) void NMI_Handler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void HardFault_Handler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void Ecall_M_Mode_Handler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void Ecall_U_Mode_Handler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void Break_Point_Handler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void SysTick_Handler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void SW_Handler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void WWDG_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void PVD_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void TAMPER_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void RTC_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void FLASH_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void RCC_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void EXTI0_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void EXTI1_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void EXTI2_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void EXTI3_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void EXTI4_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void DMA1_Channel1_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void DMA1_Channel2_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void DMA1_Channel3_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void DMA1_Channel4_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void DMA1_Channel5_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void DMA1_Channel6_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void DMA1_Channel7_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void ADC1_2_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void USB_HP_CAN1_TX_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void USB_LP_CAN1_RX0_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void CAN1_RX1_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void CAN1_SCE_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void EXTI9_5_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void TIM1_BRK_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void TIM1_UP_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void TIM1_TRG_COM_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void TIM1_CC_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void TIM2_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void TIM3_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void TIM4_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void I2C1_EV_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void I2C1_ER_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void I2C2_EV_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void I2C2_ER_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void SPI1_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void SPI2_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void USART1_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void USART2_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void USART3_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void EXTI15_10_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void RTCAlarm_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void USBWakeUp_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void USBHD_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void USBHDWakeUp_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void UART4_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
+__attribute__((weak)) void DMA1_Channel8_IRQHandler(void) __attribute__ ((weak, interrupt("WCH-Interrupt-fast"), alias ("while1_handler")));
--- system/CH32V20x/USER/ch32v20x_it.h
+++ system/CH32V20x/USER/ch32v20x_it.h
@@ -14,8 +14,65 @@
  extern "C" {
 #endif 
 
-#include "debug.h"
+// #include "debug.h"
+#include <stddef.h>
 
+void while1_handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+
+void NMI_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+void HardFault_Handler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void Ecall_M_Mode_Handler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void Ecall_U_Mode_Handler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void Break_Point_Handler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void SysTick_Handler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void SW_Handler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void WWDG_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void PVD_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void TAMPER_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void RTC_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void FLASH_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void RCC_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void EXTI0_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void EXTI1_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void EXTI2_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void EXTI3_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void EXTI4_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void DMA1_Channel1_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void DMA1_Channel2_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void DMA1_Channel3_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void DMA1_Channel4_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void DMA1_Channel5_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void DMA1_Channel6_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void DMA1_Channel7_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void ADC1_2_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void USB_HP_CAN1_TX_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void USB_LP_CAN1_RX0_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void CAN1_RX1_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void CAN1_SCE_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void EXTI9_5_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void TIM1_BRK_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void TIM1_UP_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void TIM1_TRG_COM_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void TIM1_CC_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void TIM2_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void TIM3_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void TIM4_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void I2C1_EV_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void I2C1_ER_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void I2C2_EV_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void I2C2_ER_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void SPI1_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void SPI2_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void USART1_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void USART2_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void USART3_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void EXTI15_10_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void RTCAlarm_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void USBWakeUp_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void USBHD_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void USBHDWakeUp_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void UART4_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
+void DMA1_Channel8_IRQHandler(void)  __attribute__((interrupt("WCH-Interrupt-fast")));
 
 
 #ifdef __cplusplus
