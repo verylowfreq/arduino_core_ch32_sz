1e831b6de22bfafc31840b72f84e8ece8fc76dec
Mitsumine Suzu <60875431+verylowfreq@users.noreply.github.com>
2026-04-18 10:09:58 +0900

    Cleanup comments in EEPROM library

--- libraries/EEPROM/src/EEPROM.cpp
+++ libraries/EEPROM/src/EEPROM.cpp
@@ -1,23 +1,19 @@
 /*
   EEPROM - Enables reading and writing to non-volatile storage in the processor.
-  
-  Ported by Maxint R&D to CH32, based on multiple sources, see EEPROM.h.
 
-  From the CH32V004 Data Sheet:
-   "Built-in 1920 bytes of system storage (System FLASH) for system bootloader storage (factory-cured
-   bootloader) 64 bytes are used for the system non-volatile configuration information storage area and 64 bytes
-   are used for the user select word storage area."
-  So next to 16kB of Code FLASH, the chip features 2kB Flash for boot, configuration and storage. That 64 bytes
-  user select word storage area is the area we can use to emulate on chip EEPROM memory.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
 
-  From the CH32V003 Reference Manual:
-   "The user-option bytes is solidified in FLASH and will be reloaded into the corresponding register after system
-    reset, and can be erased and programmed by the user at will. The user option bytes information block has a
-    total of 8 bytes (4 bytes for write protection, 1 byte for read protection, 1 byte for configuration options, and
-    2 bytes for storing user data), and each bit has its inverse code bit for checksum during loading."
-   These 8 bytes use 16 bytes of flash. The total storage area page is 64 bytes, leaving 48 bytes available, 
-   (including required inverse values). This is 24 bytes that can be used plus the two data0 and data1 bytes. 
-   Layout for uint8_t _data[26]: { ob[4], ob[6], ob[16...62] ].
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 #if defined(ARDUINO_ARCH_CH32) && defined(CH32V20x)
@@ -33,26 +29,12 @@ static const uint32_t EEPROM_AREA_ADDRESS = 0x08000000 + 224 * 1024 - EEPROM_SIZ
 alignas(4) static uint8_t buffer[EEPROM_SIZE];
 
 
-
-// #define OB_AVAIL_DATA_START 8  // valid for CH32V003; how about others?
-
 EEPROMClass::EEPROMClass(void) {
 }
 
 EEPROMClass::~EEPROMClass() {
   end();
 }
-
-// uint32_t EEPROMClass::ReadOptionBytes()
-// { // Read data0 and data1 option bytes
-//   // These bytes are special, as in that they are 16-bit words, each having their value in the first byte and the inverse value as second byte.
-//   // Their registers are automatically filled at boot time, so they can be read before calling EEPROM.begin();
-//    uint32_t uRes=0;
-//    uRes|=OB->Data0;
-//    uRes<<=16;
-//    uRes|=OB->Data1;
-//    return uRes;
-// }
 
 
 void EEPROMClass::begin(void)
@@ -75,13 +57,13 @@ uint8_t const * EEPROMClass::getConstDataPtr() const {
   return &_data[0];
 }
 
-uint8_t EEPROMClass::read( int const idx ) {
+uint8_t EEPROMClass::read(int const idx) {
   if(_data && idx>=0 && (size_t)idx<_size)
     return(_data[idx]);
   return(0);
 }
 
-void EEPROMClass::write( int const idx, uint8_t  const val ) {
+void EEPROMClass::write(int const idx, uint8_t  const val) {
   if(_data && idx>=0 && (size_t)idx<_size) {
     _dirty = true;
     _data[idx]=val;
@@ -110,72 +92,11 @@ bool EEPROMClass::commit()
     FLASH_ErasePage_Fast(flash_addr);
     FLASH_ProgramPage_Fast(flash_addr, (uint32_t*)(&_data[i]));
   }
-  
-  // Serial.printf("Erase page 0x%08x\r\n", EEPROM_AREA_ADDRESS);
-  // FLASH_ErasePage_Fast(EEPROM_AREA_ADDRESS);
-  // Serial.printf("Erase page 0x%08x\r\n", EEPROM_AREA_ADDRESS + FLASH_PAGE_SIZE);
-  // FLASH_ErasePage_Fast(EEPROM_AREA_ADDRESS + FLASH_PAGE_SIZE);
-  // FLASH_ProgramPage_Fast(EEPROM_AREA_ADDRESS, (uint32_t*)&_data[0]);
-  // FLASH_ProgramPage_Fast(EEPROM_AREA_ADDRESS + FLASH_PAGE_SIZE, (uint32_t*)(&_data[FLASH_PAGE_SIZE]));
 
   FLASH_Enhance_Mode(ENABLE);
   FLASH_Lock_Fast();
-
   _dirty = false;
 
-  // volatile uint16_t hold[8]; 		// array to hold reserved values while erasing
-  // // The entire 64 byte data block of the "User-selected words" will be erased
-  // // so we need to keep a copy of the reserved content for re-writing after erase.
-  // // Save a few (20) bytes code space by moving 32 bits at a time.
-  // //  hold[0]=OB->RDPR;       // reserved for read protection
-  // //  hold[1]=OB->USER;       // reserved for user settings
-  // //  hold[2]=data0;          // available user data byte (second byte holds inverse)
-  // //  hold[3]=data1;          // available user data byte (second byte holds inverse)
-  // //  hold[4]=OB->WRPR0;      // reserved for 16kB write protection
-  // //  hold[5]=OB->WRPR1;      // reserved for 16kB write protection
-  // //  hold[6]=OB->WRPR2;      // reserved value
-  // //  hold[7]=OB->WRPR3;      // reserved value
-  // uint32_t *hold32p=(uint32_t *)hold;
-  // uint32_t *ob32p=(uint32_t *)OB_BASE;
-  // hold32p[0]=ob32p[0];    // Copy RDPR and USER
-  // hold32p[1]=_data[0]+(_data[1]<<16);	// Copy in the two Data values that are not reserved
-  // hold32p[2]=ob32p[2];    // Copy WRPR0 and WEPR1
-  // hold32p[3]=ob32p[3];    // Copy reserved WRPR2 and WEPR3
-
-  // // Unlock both the general Flash and the User-selected words
-  // FLASH->KEYR = FLASH_KEY1;
-  // FLASH->KEYR = FLASH_KEY2;
-  // FLASH->OBKEYR = FLASH_KEY1;
-  // FLASH->OBKEYR = FLASH_KEY2;
-
-  // FLASH->CTLR |= CR_OPTER_Set;        // OBER RW Perform user-selected word erasure	
-  // FLASH->CTLR |= CR_STRT_Set;         // STRT RW1 Start. Set 1 to start an erase action,hw automatically clears to 0
-  // while (FLASH->STATR & FLASH_BUSY);  // Wait for flash operation to be done
-  // FLASH->CTLR &= CR_OPTER_Reset;      // Disable erasure mode	
-
-  // // Write the held values back one-by-one
-  // FLASH->CTLR |= CR_OPTPG_Set;   			// OBG  RW Perform user-selected word programming
-  // uint16_t *ob16p=(uint16_t *)OB_BASE;
-  // for (int i=0;i<sizeof(hold)/sizeof(hold[0]); i++) {
-  //   ob16p[i]=hold[i];
-  //   while (FLASH->STATR & FLASH_BUSY);	// Wait for flash operation to be done
-  // }
-
-  // // Then write the remainder of the data block
-  // // Note that when a byte is written as a word, it automatically gets its inverse value as second byte
-  // if(_data && _size)
-  // {
-  //   uint16_t *ob16p=(uint16_t *)OB_BASE;
-  //   for (int i=2;i<_size; i++) {
-  //     if(_data[i]!=0xFF) {
-  //       ob16p[OB_AVAIL_DATA_START+(i-2)]=_data[i];
-  //       while (FLASH->STATR & FLASH_BUSY);	// Wait for flash operation to be done
-  //     }
-  //   }
-  // }
-    
-  // FLASH->CTLR &= CR_OPTPG_Reset;			// Disable programming mode
-  // FLASH->CTLR|=CR_LOCK_Set;				// Lock flash memories again
   return(true);
 }
 
--- libraries/EEPROM/src/EEPROM.h
+++ libraries/EEPROM/src/EEPROM.h
@@ -1,18 +1,6 @@
 /*
   EEPROM - Enables reading and writing to non-volatile storage in the processor.
-  
-  Uses the Option bytes area in Flash to emulate EEPROM.
-  On the CH32V003 there are 2+24 bytes available. 
-  The first two bytes are Option bytes data0 and data1.
-  All bytes are copied to a 26-byte long byte array in RAM.
-  After changing a value the commit() method is used to write flash.
 
-  Ported by Maxint R&D to CH32, based on multiple sources.
-  Tested on CH32V003 using Arduino IDE 2.3.2 and OpenWCH core 1.0.4. 
-  Includes code from Option Data example of CH32V003fun by CNLOHR.
-  Arduino original copyright (c) 2006 David A. Mellis.  All right reserved.
-  ESP8266 version copyright (c) 2014 Ivan Grokhotkov. All rights reserved.
-  
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
@@ -32,30 +20,6 @@
 #define EEPROM_h
 #include <Arduino.h>
 
-// Required definitions copied from /system/CH32V00x/SRC/Peripheral/src/ch32v00x_flash.c
-/* Flash Control Register bits */
-//#define CR_PG_Set                  ((uint32_t)0x00000001)
-//#define CR_PG_Reset                ((uint32_t)0xFFFFFFFE)
-//#define CR_PER_Set                 ((uint32_t)0x00000002)
-//#define CR_PER_Reset               ((uint32_t)0xFFFFFFFD)
-//#define CR_MER_Set                 ((uint32_t)0x00000004)
-//#define CR_MER_Reset               ((uint32_t)0xFFFFFFFB)
-// #define CR_OPTPG_Set               ((uint32_t)0x00000010)
-// #define CR_OPTPG_Reset             ((uint32_t)0xFFFFFFEF)
-// #define CR_OPTER_Set               ((uint32_t)0x00000020)
-// #define CR_OPTER_Reset             ((uint32_t)0xFFFFFFDF)
-// #define CR_STRT_Set                ((uint32_t)0x00000040)
-// #define CR_LOCK_Set                ((uint32_t)0x00000080)
-//#define CR_PAGE_PG                 ((uint32_t)0x00010000)
-//#define CR_PAGE_ER                 ((uint32_t)0x00020000)
-//#define CR_BUF_LOAD                ((uint32_t)0x00040000)
-//#define CR_BUF_RST                 ((uint32_t)0x00080000)
-
-/* FLASH Keys */
-//#define RDP_Key                    ((uint16_t)0x00A5)
-// #define FLASH_KEY1                 ((uint32_t)0x45670123)
-// #define FLASH_KEY2                 ((uint32_t)0xCDEF89AB)
-
 
 class EEPROMClass {
   public:
@@ -67,8 +31,8 @@ class EEPROMClass {
     uint8_t * getDataPtr();
     uint8_t const * getConstDataPtr() const;
 
-    uint8_t read( int const idx );
-    void write( int const idx, uint8_t const val);     // requires commit() to make data stick
+    uint8_t read(int const idx);
+    void write(int const idx, uint8_t const val);     // requires commit() to make data stick
     void update(int const idx, uint8_t const val) { write(idx, val); }
     void erase(void);     // requires commit() to make data stick
 
@@ -110,4 +74,4 @@ extern EEPROMClass EEPROM;
 #endif
 
 
-#endif		// #ifndef EEPROM_h
+#endif
